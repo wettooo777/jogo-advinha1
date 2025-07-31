06e740fc8e536ec0439c91d2e2e7903a90ac28fb
wettooo777 <welliton.souza.oliveira@escola.pr.gov.br>
2025-07-31 14:49:52 +0000

    parte 2 do restio do jogo.py

--- jogo.py
+++ jogo.py
@@ -13,7 +13,7 @@ print("#  #       #          #")
 print("#  # # # # #          #")
 print("#  #       #          #")
 print("#  #       #          #")
-print("#                     #")
+print("# ------------------- #")
 print("#  #                  #")
 print("#  #     ###########  #")
 print("#  #                  #")
@@ -25,3 +25,19 @@ print("#######################")
 numeroSecreto = random.randrange(0,100)
 totaltentativas = 0
 pontos = 100
+
+print("Qual o níveis de dificuldade?")
+print("1-(fácil)-2-(médio)-3-difícil")
+nivel = int(input("Escolha um nível: "))
+if nivel == 1:
+    print ("você é um mariquinha! (FÁCIL)")
+    print ("Agora você tem 20 tentativas!")
+    totaltentativas = 20
+elif nivel == 2:
+    print("você não especial (MÉDIO)")
+    print ("Agora você tem 10 tentativas!")
+    totaltentativas = 10
+else:
+    print("você é burro? (DÍFICIL)")
+    print ("Agora você tem 5 tentativas!")
+    totaltentativas = 5
