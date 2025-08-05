c295dc9d31456267ae0540304bf4623b38a9c354
wettooo777 <welliton.souza.oliveira@escola.pr.gov.br>
2025-08-05 23:31:59 +0000

    PELO AMOR DE DEUS ,NÃO SEI OQUE FIZ SLA

--- jogo.py
+++ jogo.py
@@ -1,10 +1,5 @@
 import random
 
-print("#######################")
-print("# jogo de adivinhação #")
-print("#     Welliton h.     #")
-print("#######################")
-
 RED = '\033[91m'
 GREEN = '\033[92m'
 YELLOW = '\033[93m'
@@ -14,13 +9,19 @@ CYAN = '\033[96m'
 WHITE = '\033[97m'
 RESET = '\033[0m'
 
+print(f"{RED}#######################{RESET}")
+print(f"{RED}#{RESET} {MAGENTA}jogo de adivinhação{RESET} {RED} #{RESET}")
+print(f"{RED}#{RESET} {BLUE}    Welliton h.    {RESET} {RED}#{RESET}")
+print(f"{RED}#######################{RESET}")
+
+
 numeroSecreto = random.randrange(0,100)
 totaltentativas = 0
 pontos = 1000
 
 print(f"{YELLOW}Qual o níveis de dificuldade?{RESET}")
-print("1-(fácil)-2-(médio)-3-(difícil)-4-(treino)")
-nivel = int(input("Escolha um nível: "))
+print(f"1-{BLUE}(fácil){RESET}-2-{YELLOW}(médio){RESET}-3-{RED}(difícil){RESET}-4-{MAGENTA}(treino){RESET}")
+nivel = int(input(f"Escolha um nível: "))
 
 if nivel == 1:
     print (f"{BLUE}você deve estar brincando! (FÁCIL){RESET}")
@@ -43,15 +44,18 @@ elif nivel == 4:
     print(f"Número secreto: {numeroSecreto}")
 
 else:
-    print("Essa dificuldade não existe")
+    print(f"{RED}Essa dificuldade não existe{RED}")
 
 for rodada in range (1, totaltentativas +1):
-    print ("Tentativas {} de {}".format(rodada, totaltentativas))
+
+    barra = "#" * rodada
+    print (f"{MAGENTA} {[barra]} {RESET} ({rodada}/{totaltentativas}) {BLUE}Tentativas usadas{RESET}")
     chute_str = input("Digite um número entre 1 e 100: ")
+
     chute = int(chute_str)
 
     if (chute < 1 or chute > 100):
-        print("Número invalido")
+        print(f"{RED}Número invalido{RESET}")
         continue
     
     acertou = chute == numeroSecreto
@@ -59,16 +63,16 @@ for rodada in range (1, totaltentativas +1):
     menor = chute < numeroSecreto
 
     if (acertou):
-        print(f"Você acertou e fez {pontos}! ")
+        print(f"{GREEN}Você acertou e fez {pontos}{RESET}! ")
         break
 
     else:
         if(maior):
-            print("Você errou!Seu chute foi maior que número secreto")
+            print(f"{RED}Você errou!Seu chute foi maior que número secreto{RESET}")
         elif(menor):
-            print("Você errou! Seu chute foi menor que número secreto")
+            print(f"{RED}Você errou! Seu chute foi menor que número secreto{RESET}")
 
             pontosperdidos = abs(numeroSecreto - chute)
             pontos = pontos - pontosperdidos
 
-print("Fim de jogo! O número era ",numeroSecreto)
+print (f"{CYAN}Fim de jogo! O número era: {RESET}",numeroSecreto)
